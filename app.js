46120a2c84fb8893c72deba9e4b0b51cfd4cc38f
Zach Mabie <zzmabie@yahoo.com>
2022-08-11 16:44:07 +0800

    second commit

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authJwt = require("./helpers/jwt");
-const errorHandler = require("./helpers/error-handler");
+// const authJwt = require("./helpers/jwt");
+// const errorHandler = require("./helpers/error-handler");
 
 require("dotenv/config");
+const api = process.env.API_URL;
+
 
 app.use(cors());
 app.options("*", cors());
@@ -15,5 +17,23 @@ app.options("*", cors());
 //middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
-app.use(authJwt());
-app.use(errorHandler);
+// app.use(authJwt());
+// app.use(errorHandler);
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    dbName: 'genesis-database'
+})
+.then(()=>{
+    console.log('Database connection is ready...')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+//APP.Listen starts up the API on a server, specifying the port as it's first parameter
+app.listen(3000, ()=>{
+    console.log("Serving running on 127.0.0.1:3000");
+    console.log(api);
+});
