0d16ffc6dc38d48e63222446f383bd2b8a1221d7
Zach Elliott <zach.elliott9@gmail.com>
2022-08-11 16:30:37 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
+
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(authJwt());
+app.use(errorHandler);
