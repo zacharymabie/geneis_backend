6f714b5542145fcdf8c06e7a93a9001ad6eeb4a9
Zach Mabie <zzmabie@yahoo.com>
2022-08-12 18:20:47 +0800

    changes to error-handler.js, jwt.js, users.js, app,js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const authJwt = require("./helpers/jwt");
-// const errorHandler = require("./helpers/error-handler");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 require("dotenv/config");
 const api = process.env.API_URL;
@@ -16,8 +16,8 @@ app.options("*", cors());
 //middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
-// app.use(authJwt());
-// app.use(errorHandler);
+app.use(authJwt());
+app.use(errorHandler);
 
 //routes
 const usersRouter = require('./routes/users.js');
--- routes/users.js
+++ routes/users.js
@@ -24,23 +24,12 @@ router.get(`/:id`, async (req,res)=>{
     res.status(200).send(user);
 });
 
-router.post('/',async (req,res)=>{
-    let user = new User({
-        username: req.body.username,
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        isAdmin: req.body.isAdmin,
-        age: req.body.age,
-        weight: req.body.weight,
-        height: req.body.height,
-        profilePic: req.body.profilePic
-    })
-    user = await user.save();
-    if(!user)
-        return res.status(400).send('User cannot be created');
-
-    res.send(user);
+router.get(`/get/count`, async (req,res)=>{
+    const userCount = await User.countDocuments(count => count).clone();
+    if(!userCount){
+        res.status(500).json({'success': false})
+    } 
+    res.send({userCount: userCount});
 });
 
 router.post('/login', async (req,res)=>{
@@ -54,6 +43,7 @@ router.post('/login', async (req,res)=>{
         const token = jwt.sign(
             {
             userId: user.id,
+            isAdmin:user.isAdmin
             }, 
             secret,
             {expiresIn: '1d'}
@@ -85,6 +75,41 @@ router.post('/register', async (req,res)=>{
 
     res.send(user);
 
+})
+
+router.put('/:id',async (req,res)=>{
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            username: req.body.username,
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+            isAdmin: req.body.isAdmin,
+            age: req.body.age,
+            weight: req.body.weight,
+            height: req.body.height,
+            profilePic: req.body.profilePic
+        },
+        {new:true}
+    )
+    if(!user)
+    return res.status(400).send('user cannot be created');
+
+    res.send(user);
+})
+
+
+router.delete('/:id', (req,res)=>{
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user){
+            return res.status(200).json({success:true, message:'user is deleted'})
+        } else {
+            return res.status(404).json({success:false,message:"user not found"})
+        }
+    }).catch(err=>{
+        return res.status(400).json({success:false, error:err})
+    })
 })
 
 module.exports = router;
