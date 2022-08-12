4787767794e77845b983197531ddfb6ee14d266b
ZachElliott4 <83951986+ZachElliott4@users.noreply.github.com>
2022-08-12 23:53:17 +0800

    Updated program.js

--- models/program.js
+++ models/program.js
@@ -1,1 +1,54 @@
+const mongoose = require("mongoose");
 
+const programSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  workouts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workout",
+      required: true,
+    },
+  ],
+  level: {
+    type: Number,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+programSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+programSchema.set("toJSON", {
+  virtuals: true,
+});
+
+exports.Program = mongoose.model("Program", programSchema);
