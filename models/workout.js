7c2489e3b710fbed0bc0b6468220295771bcef5a
Zach Elliott <zach.elliott9@gmail.com>
2022-08-12 23:49:43 +0800

    Added updated program model and changed workout model

--- models/workout.js
+++ models/workout.js
@@ -21,26 +21,6 @@ const workoutSchema = mongoose.Schema({
       required: true,
     },
   ],
-  level: {
-    type: Number,
-    default: 0,
-  },
-  rating: {
-    type: Number,
-    default: 0,
-  },
-  users: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  comments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
-    },
-  ],
 });
 
 workoutSchema.virtual("id").get(function () {
