99fac3ac91744573d6358a4bcc954ea74cc45e00
Zach Mabie <zzmabie@yahoo.com>
2022-08-25 13:34:06 +0800

    jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -13,7 +13,7 @@ function authJwt() {
       path: [
         { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
         { url: /\/api\/v1\/posts(.*)/, methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"] },
-        {url: /\/api\/v1\/users(.*)/, methods:['GET', 'OPTIONS', 'PUT']},
+        {url: /\/api\/v1\/users(.*)/, methods:['GET', 'OPTIONS', 'PUT', 'DELETE']},
         `${api}/users/login`,
         `${api}/users/register`,
         `${api}/users/get/count`,
